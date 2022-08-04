fefce90691730674f419326096f55bd85b866b3a
Burton Rodman <burtonrodman@hotmail.com>
2022-08-03 23:52:49 -0400

    get 2 independent i2c buses running on arbitrary pins

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, SoftI2C
 from ssd1306 import SSD1306_I2C
 import utime
 
-class Display:
-    def __init__(self, sda, scl, text):
-        self.sda = sda
-        self.scl = scl
-        self.text = text
-
 w = 64
 h = 32
-
 x = 0
 
-displays = [
-    Display(16, 17, "16/17"),
-    Display(20, 21, "20/21")
-]
+i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=200000)
+addr = i2c.scan()[0]
+oled = SSD1306_I2C(w, h, i2c, addr)
+
+si2c = SoftI2C(scl=Pin(21), sda=Pin(20))
+saddr = si2c.scan()[0]
+soled = SSD1306_I2C(w, h, si2c, saddr)
 
 while True:
     x += 1
-    print(x)
-    for d in displays:
-        i2c = I2C(0, scl=Pin(d.scl), sda=Pin(d.sda), freq=200000)
-        addr = i2c.scan()[0]
-        oled = SSD1306_I2C(w, h, i2c, addr)
-        oled.fill(0)
-        oled.text(d.text, 0, 0)
-        oled.text(str(x), 0, 8)
-        oled.show()
 
-        Pin(d.scl, Pin.IN, Pin.PULL_DOWN)
-        Pin(d.sda, Pin.IN, Pin.PULL_DOWN)
-        
-        utime.sleep(1)
+    oled.fill(0)
+    oled.text("BUS13/14", 0, 0)
+    oled.text("08 BURT ", 0, 8)
+    oled.text("LAV GRN ", 0, 16)
+    oled.text("XXXXXXXX", 0, 24)
+    oled.show()
+
+    soled.fill(0)
+    soled.text("21/20", 0, 0)
+    soled.text(str(x), 0, 8)
+    soled.show()
+
+    utime.sleep_ms(1)
