153088a092e7b9b0e255d38fcf64f838520cac85
Burton Rodman <burtonrodman@hotmail.com>
2022-08-05 08:55:32 -0400

    refactor: drawTextLines

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from machine import Pin, I2C, SoftI2C
 from ssd1306 import SSD1306_I2C
 import utime
+import gc
+
+def drawTextLines(oled, text):
+    y = 0
+    oled.fill(0)
+    for line in text:
+        oled.text(line, 0, y); y += 8;
+    oled.show()
 
 w = 64
 h = 32
@@ -14,19 +22,15 @@ si2c = SoftI2C(scl=Pin(21), sda=Pin(20))
 saddr = si2c.scan()[0]
 soled = SSD1306_I2C(w, h, si2c, saddr)
 
+ei2c = SoftI2C(scl=Pin(13), sda=Pin(12))
+eaddr = ei2c.scan()[0]
+eoled = SSD1306_I2C(w, h, ei2c, eaddr)
+
 while True:
     x += 1
 
-    oled.fill(0)
-    oled.text("BUS13/14", 0, 0)
-    oled.text("08 BURT ", 0, 8)
-    oled.text("LAV GRN ", 0, 16)
-    oled.text("XXXXXXXX", 0, 24)
-    oled.show()
+    drawTextLines(oled, ["BUS13/14", "08 BURT ", "LAV GRN ", "drawBus"])
+    drawTextLines(soled, ["21/20", "free mem", str(gc.mem_free()), "drawText"])
+    drawTextLines(eoled, ["13/12", str(x), "", "drawText"])
 
-    soled.fill(0)
-    soled.text("21/20", 0, 0)
-    soled.text(str(x), 0, 8)
-    soled.show()
-
-    utime.sleep_ms(1)
+    utime.sleep_ms(1)  # keep device responsive to code upload, etc...
